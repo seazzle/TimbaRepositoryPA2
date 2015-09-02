72790a2cc11017bcf16f31cb41a169cab5a4d3f3
seazzle <dev.seazzle@gmail.com>
2015-09-02 09:56:44 +0200

    logging Antwort Objekt

--- Timba/WebContent/js/viewModels/reporting/reportingController.js
+++ Timba/WebContent/js/viewModels/reporting/reportingController.js
@@ -22,6 +22,7 @@ angular.module('Reporting').config(function($sceProvider, $httpProvider) {
 			url : serviceURL + '/report/reportConfig/' + $rootScope.user,
 			method : "GET",
 		}).success(function(data) {
+			$log.debug("Antwort-Objekt: "+angular.toJson(data.content));
 			if (data.success == true) {
 				$scope.showErrorBox = false;
 				$scope.reportConfig = data.content;
